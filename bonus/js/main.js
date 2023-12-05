0e102b128985b782dcf41881f719827b5a5a5232
AndreaGul <andreagulino99@gmail.com>
2023-12-05 20:29:59 +0100

    feat:bonus

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -6,59 +6,30 @@ createApp({
   data() {
     return {
       apiEmaiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
-      email1: null,
-      email2: null,
-      email3: null,
-      email4: null,
-      email5: null,
-      email6: null,
-      email7: null,
-      email8: null,
-      email9: null,
-      email10: null,
+      emails: [],
+      email: null,
+
       maxNumber: 10,
     };
   },
-  // methods: {
-  //   randomEmail(index) {
-  //     if (index < this.maxNumber) {
-  //       axios.get(this.apiEmaiUrl).then((response) => {
-  //         console.log(response.data);
-  //         this.emails.push = response.data.response;
-  //       });
-  //     }
-  //   },
-  // },
+  methods: {
+    // randomEmail() {
+    //   axios.get(this.apiEmaiUrl).then((response) => {
+    //     console.log(response.data);
+    //     this.email = response.data.response;
+    //   });
+    // },
+
+    listaEmail() {
+      for (let i = 0; i < this.maxNumber; i++) {
+        axios.get(this.apiEmaiUrl).then((response) => {
+          this.email = response.data.response;
+          this.emails.push(this.email);
+        });
+      }
+    },
+  },
   created() {
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email1 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email2 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email3 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email4 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email5 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email6 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email7 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email8 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email9 = response.data.response;
-    });
-    axios.get(this.apiEmaiUrl).then((response) => {
-      this.email10 = response.data.response;
-    });
+    this.listaEmail();
   },
 }).mount('#app');
